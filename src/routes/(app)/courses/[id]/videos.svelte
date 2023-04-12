380a1b06dfd5ed50622dc0f62a6f4db6d80c2de6
Onojeta Brown <onosbrown.saved@gmail.com>
2023-04-12 17:10:39 +0100

    update

--- src/routes/(app)/courses/[id]/videos.svelte
+++ src/routes/(app)/courses/[id]/videos.svelte
@@ -1,12 +1,12 @@
 <div>
-    <!-- ADD VIDEO -->
-    <div class="flex align-end justify-between  my-2 md:p-2 md:pr-10">
-      <h1 class="text-lg my-5 font-bold sm:text-xl">COURSE VIDEOS</h1>
-              <!-- <p class="font-bold uppercase mx-2">Create Course</p> -->
-          <label for="my-modal" class="btn font-light uppercase mx-2 sm:font-bold">
-              Add Video 
-              <div class="i-bx:bxs-add-to-queue text-xl text-primary mx-2 sm:text-3xl"></div>
-          </label>
+  <!-- ADD VIDEO -->
+  <div class="flex align-end justify-between my-2 md:p-2 md:pr-10">
+    <h1 class="text-lg my-5 font-bold sm:text-xl">COURSE VIDEOS</h1>
+    <!-- <p class="font-bold uppercase mx-2">Create Course</p> -->
+    <label for="my-modal" class="btn font-light uppercase mx-2 sm:font-bold">
+      Add Video
+      <div class="i-bx:bxs-add-to-queue text-xl text-primary mx-2 sm:text-3xl" />
+    </label>
   </div>
 </div>
 
@@ -18,92 +18,96 @@
     <h3 class="font-bold text-lg">Create A New Course</h3>
 
     <form action="#">
-        <div class="mt-4 grid grid-cols-4">
-           
-            <!-- input -->
-          <div class="relative col-span-4 sm:col-span-2 mr-2 mb-4">
-              <input
-                name="title"
-                type="text"
-                id="title"
-                class=" input input-bordered floating-input peer focus:border-accent-focus"
-                placeholder=" "
-                required
-              />
-              <label for="title" class="floating-label peer-focus:text-accent-focus">
-                Video Title
-              </label>
-            </div>
-            <!-- end of input -->
-
-             <!-- input -->
-             <div class="relative col-span-4 sm:col-span-2 mr-2 mb-4">
-              <input
-                name="category"
-                type="text"
-                id="category"
-                class=" input input-bordered floating-input peer focus:border-accent-focus"
-                placeholder=" "
-                required
-              />
-              <label for="category" class="floating-label peer-focus:text-accent-focus">
-                Video Category
-              </label>
-            </div>
-            <!-- end of input -->
-
-            <!-- input -->
-            <textarea class="textarea textarea-bordered col-span-4 sm:col-span-4 mr-2 mb-4" name="description" id="description" placeholder="Video Description"></textarea>
-            <!-- end of input -->
-
-              <div class="relative col-span-4 sm:col-span-4 mr-2 mb-4">
-                <div class="form-control w-full max-w-xs">
-                    <!-- svelte-ignore a11y-label-has-associated-control -->
-                    <label class="label"><span class="label-text">Select Lesson Video</span></label>
-                    <input type="file" name="coverImageUrl" class="file-input file-input-bordered w-full max-w-xs" />
-                  </div>
-              </div>
-              <!-- end of input -->
-
-
-
+      <div class="mt-4 grid grid-cols-4">
+        <!-- input -->
+        <div class="relative col-span-4 sm:col-span-2 mr-2 mb-4">
+          <input
+            name="title"
+            type="text"
+            id="title"
+            class=" input input-bordered floating-input peer focus:border-accent-focus"
+            placeholder=" "
+            required
+          />
+          <label for="title" class="floating-label peer-focus:text-accent-focus">
+            Video Title
+          </label>
         </div>
+        <!-- end of input -->
 
-          
-        <div class="modal-action">
-            <button class="btn">Create</button>
-            <label for="my-modal" class="btn">Cancel</label>
+        <!-- input -->
+        <div class="relative col-span-4 sm:col-span-2 mr-2 mb-4">
+          <input
+            name="category"
+            type="text"
+            id="category"
+            class=" input input-bordered floating-input peer focus:border-accent-focus"
+            placeholder=" "
+            required
+          />
+          <label for="category" class="floating-label peer-focus:text-accent-focus">
+            Video Category
+          </label>
         </div>
+        <!-- end of input -->
+
+        <!-- input -->
+        <textarea
+          class="textarea textarea-bordered col-span-4 sm:col-span-4 mr-2 mb-4"
+          name="description"
+          id="description"
+          placeholder="Video Description"
+        />
+        <!-- end of input -->
+
+        <div class="relative col-span-4 sm:col-span-4 mr-2 mb-4">
+          <div class="form-control w-full max-w-xs">
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label class="label"><span class="label-text">Select Lesson Video</span></label>
+            <input
+              type="file"
+              name="coverImageUrl"
+              class="file-input file-input-bordered w-full max-w-xs"
+            />
+          </div>
+        </div>
+        <!-- end of input -->
+      </div>
+
+      <div class="modal-action">
+        <button class="btn">Create</button>
+        <label for="my-modal" class="btn">Cancel</label>
+      </div>
     </form>
   </div>
 </div>
 
 <div class="mx-2 mb-12 flex flex-col md:flex-row flex-wrap">
   <!-- card 1 -->
-<div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
-  <figure>
-    <video class="w-full" controls>
-      <source src="#" type="video/mp4">
-      <source src="#" type="video/ogg">
-    Your browser does not support the video tag.
-    </video>
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">
-      Lesson 1
-      <div class="badge badge-secondary">NEW</div>
-    </h2>
-    <p class="text-sm">Introduction to Apologetics</p>
+  <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
+    <figure>
+      <video class="w-full" controls>
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        Lesson 1
+        <div class="badge badge-secondary">NEW</div>
+      </h2>
+      <p class="text-sm">Introduction to Apologetics</p>
+    </div>
   </div>
-</div>
 
   <!-- card 2 -->
   <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
     <figure>
       <video class="w-full" controls>
-        <source src="#" type="video/mp4">
-        <source src="#" type="video/ogg">
-      Your browser does not support the video tag.
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
       </video>
     </figure>
     <div class="card-body">
@@ -115,13 +119,13 @@
     </div>
   </div>
 
-   <!-- card 3 -->
-   <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
+  <!-- card 3 -->
+  <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
     <figure>
       <video class="w-full" controls>
-        <source src="#" type="video/mp4">
-        <source src="#" type="video/ogg">
-      Your browser does not support the video tag.
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
       </video>
     </figure>
     <div class="card-body">
@@ -137,9 +141,9 @@
   <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
     <figure>
       <video class="w-full" controls>
-        <source src="#" type="video/mp4">
-        <source src="#" type="video/ogg">
-      Your browser does not support the video tag.
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
       </video>
     </figure>
     <div class="card-body">
@@ -155,9 +159,9 @@
   <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
     <figure>
       <video class="w-full" controls>
-        <source src="#" type="video/mp4">
-        <source src="#" type="video/ogg">
-      Your browser does not support the video tag.
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
       </video>
     </figure>
     <div class="card-body">
@@ -169,23 +173,21 @@
     </div>
   </div>
 
-    <!-- card 6 -->
-<div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
-  <figure>
-    <video class="w-full" controls>
-      <source src="#" type="video/mp4">
-      <source src="#" type="video/ogg">
-    Your browser does not support the video tag.
-    </video>
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">
-      Lesson 6
-      <div class="badge badge-secondary">NEW</div>
-    </h2>
-    <p class="text-sm">Introduction to Apologetics</p>
+  <!-- card 6 -->
+  <div class="mx-2 my-3 card w-full bg-base-100 shadow-xl md:w-72">
+    <figure>
+      <video class="w-full" controls>
+        <source src="#" type="video/mp4" />
+        <source src="#" type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        Lesson 6
+        <div class="badge badge-secondary">NEW</div>
+      </h2>
+      <p class="text-sm">Introduction to Apologetics</p>
+    </div>
   </div>
-</div>
-
-
 </div>
